8a8df911a7a1f143c76fee66df2ff5df92f27e79
akhu <anthony.dacr@gmail.com>
2019-12-03 11:37:02 +0100

    Added Readme + Objectives

--- app/src/main/java/com/pickle/punktual/MainActivity.kt
+++ app/src/main/java/com/pickle/punktual/MainActivity.kt
@@ -8,5 +8,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //1. Handle login
+        //2. Ask for location permissions
+        //3. Handle location settings
+        //4. Create geofence
+        //5. Start to listen for location changes
+        //6. Create Notification when entering the fence
+        //6. Implement google map to better see things
+        //7. Put marker on Map for the papeterie and current user
+        //8. Communicate with server to log arrival date time + user
+        //9. Handle external notification from FCM
+        //10. Improvments : Google Login with profile picture
+        //11. Improvments : Some statistics with Android Chart
+        //12. Improvments : Detecting the type of transport user for a user + send to server
+
     }
 }
